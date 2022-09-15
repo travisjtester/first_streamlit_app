60932307ee2727d78549df9ded043ae937793904
Travis Tester <88358061+travisjtester@users.noreply.github.com>
2022-09-15 15:01:24 -0500

    Update streamlit_app.py
    added in database and schema to table

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ import snowflake.connector
 # test query on new snowflake connection in Streamlit
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
 streamlit.dataframe(my_data_rows)
